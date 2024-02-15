6881c80e2b2e9c5e50963873df962b88ba579f52
blobbybilb <blobbybilb@skiff.com>
2024-02-15 14:34:37 -0800

    save

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-import web_dashboard as wd
 import random
+
+# import network
+
+# wifi = network.WLAN(network.STA_IF)
+# wifi.active(True)
+# wifi.connect("electronics-workshop", "elecwork123")
+
+# while not wifi.isconnected():
+#     print(wifi.status())
+
+import web_dashboard as wd
 
 wd.id = input("ID: ")
 wd.init_grid(10)
 wd.init_log()
 
+i = 0
 while ...:
     # wd.log(str(random.random())) # stress test
-    # wd.log(input("> ")) # send from REPL test
+    i += 1
+    wd.log(str(i))
+    print(i)
+    # wd.log(input("> "))  # send from REPL test
     ...
