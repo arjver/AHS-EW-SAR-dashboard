786f71458b565357f15c1b0dda25211abc2fc0af
blobbybilb <notablob@outlook.com>
2024-03-12 19:51:43 -0700

    Update helpers.ex

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -1,5 +1,5 @@
 defmodule Helpers do
-  @tz_offset -8
+  @tz_offset -7 # -8 for PST, -7 for PST with daylight saving
 
   def get_time do
     DateTime.utc_now() |> DateTime.add(@tz_offset, :hour) |> DateTime.to_string()
